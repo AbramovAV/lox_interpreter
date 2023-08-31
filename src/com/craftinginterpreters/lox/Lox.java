d3331e8b8a4866a00f048989d643fe4a8d1aca11
Alexey Abramov <abitualex@gmail.com>
2023-08-31 15:36:14 +0300

    fixed inconsistent code formatting

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -10,13 +10,14 @@ import java.util.List;
 
 public class Lox {
     static boolean hadError = false;
+
     public static void main(String[] args) throws IOException {
-            if (args.length > 1) {
+        if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
-            System.exit(64); 
-            } else if (args.length == 1) {
+            System.exit(64);
+        } else if (args.length == 1) {
             runFile(args[0]);
-            } else {
+        } else {
             runPrompt();
         }
     }
@@ -25,28 +26,30 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         // Indicate an error in the exit code.
-        if (hadError) System.exit(65);
+        if (hadError)
+            System.exit(65);
     }
 
     private static void runPrompt() throws IOException {
-            InputStreamReader input = new InputStreamReader(System.in);
-            BufferedReader reader = new BufferedReader(input);
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
 
-            for (;;) { 
+        for (;;) {
             System.out.print("> ");
             String line = reader.readLine();
-            if (line == null) break;
+            if (line == null)
+                break;
             run(line);
             hadError = false;
         }
     }
 
     private static void run(String source) {
-            Scanner scanner = new Scanner(source);
-            List<Token> tokens = scanner.scanTokens();
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
 
-            // For now, just print the tokens.
-            for (Token token : tokens) {
+        // For now, just print the tokens.
+        for (Token token : tokens) {
             System.out.println(token);
         }
     }
@@ -56,9 +59,9 @@ public class Lox {
     }
 
     private static void report(int line, String where,
-                                String message) {
+            String message) {
         System.err.println(
-            "[line " + line + "] Error" + where + ": " + message);
+                "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
 }
